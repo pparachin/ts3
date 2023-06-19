af207c79bb05acd0f5e90da1930d3ba4ade43705
Petr Parachin <petr.parachin@gmail.com>
2023-06-19 23:13:58 +0200

    Change config for teamspeak connection

--- ts3_connection.js
+++ ts3_connection.js
@@ -2,11 +2,11 @@ const { TeamSpeak, QueryProtocol} = require("ts3-nodejs-library")
 const ts_pass = require("./ts_pass")
 
 const ts = new TeamSpeak({
-    host: "81.0.217.180",
+    host: "172.104.243.185",
     protocol: QueryProtocol.RAW, //optional
-    queryport: 10018, //optional
-    serverport: 6516,
-    username: "nodejs",
+    queryport: 10011, //optional
+    serverport: 9987,
+    username: "serveradmin",
     password: ts_pass,
     nickname: "TS3-bot",
     readyTimeout: 10000
